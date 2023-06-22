fea0e12a9a91b2c8c9be837a59d70124d08611d0
JarodBoshoff <jarodnboshoff@gmail.com>
2023-06-23 00:18:45 +0200

    updated

--- JavaScript2/johannes.js
+++ JavaScript2/johannes.js
@@ -2,5 +2,5 @@ private firstname = "Johannes"
 private surname = "Potgieter"
 public role = "Intern"
 
-private display= firstname + " " + surname + " (" + role + ")"
+private display= firstname + surname + role
 document.querySelector('#johannes').innerText = display
---
+++ JavaScript2/nwabisa.js
@@ -0,0 +1,6 @@
+private firstname = "Nwabisa"
+private surname = "Gabe"
+public role = "CEO"
+
+private display = firstname +  surname + role
+document.querySelector('#Nwabisa').innerText = display
