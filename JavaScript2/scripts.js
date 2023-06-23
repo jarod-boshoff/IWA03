dd5440329ccd6a7aaeb03d1521c3df47b8978ccf
JarodBoshoff <jarodnboshoff@gmail.com>
2023-06-23 02:54:57 +0200

    updated

--- JavaScript2/scripts.js
+++ JavaScript2/scripts.js
@@ -1,1 +1,5 @@
-console.log('Roles:', nwabisa.role, johanes.role, alex.role)
+import { firstname, surname, role as firstname1, surname1, role1} from './alex'
+import { firstname, surname, role as firstname2, surname2, role2} from './johannes'
+import { firstname, surname, role as firstname3, surname3, role3} from './nwabisa'
+
+console.log(role)
